81039455a72844ce674c10307c6db84f1f99bf19
Brandon Everell <brandoneverell@hotmail.com>
2018-02-20 14:17:03 -0800

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -1,40 +1,48 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
 
-const {generateMessage} = require('./utils/message');
-const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 5000
+
+const { generateMessage, generateLocationMessage } = require("./utils/message");
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 5000;
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-  console.log('New user connected');
+io.on("connection", socket => {
+  console.log("New user connected");
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App!'));
+  socket.emit(
+    "newMessage",
+    generateMessage("Admin", "Welcome to the chat App!")
+  );
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined!'));
+  socket.broadcast.emit(
+    "newMessage",
+    generateMessage("Admin", "New User Joined!")
+  );
 
-  socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server.');
+  socket.on("createMessage", (message, callback) => {
+    console.log("createMessage", message);
+    io.emit("newMessage", generateMessage(message.from, message.text));
+    callback("This is from the server.");
+  });
 
+  socket.on("createLocationMessage", coords => {
+    io.emit("newLocationMessage",
+      generateLocationMessage("Admin", coords.latitude, coords.longitude));
+  });
 
-});
-  socket.on('disconnect', () => {
-    console.log('User was disconnected');
+  socket.on("disconnect", () => {
+    console.log("User was disconnected");
   });
 });
 
-
-
-
 server.listen(5000, () => {
   console.log(`Server is running on port ${port} `);
-})
+});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 let expect = require('expect');
 
-let {generateMessage} = require('./message')
+let {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -12,3 +12,17 @@ describe('generateMessage', () => {
     expect(message).toInclude({from, text})
   })
 });
+
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    let from = 'Deb';
+    let latitude = 15;
+    let longitude = 19;
+    let url = 'https://www.google.com/maps?q=15,19';
+    let message = generateLocationMessage(from, latitude, longitude)
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url})
+  })
+})
